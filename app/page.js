8774ef6e0d2c551c1b795da2fd59e53bfe9d8bc1
Rayen-123 <rayen.aburto.caamano@gmail.com>
2025-10-01 18:22:26 -0300

    avance 1

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
 export default function Page() {
 	return (
 		<>
-			<h1>Hello, Next.js!</h1>
+            <div class="encabezado">
+                <div class="texto_encabezado">Encabezado</div>
+            </div>
+            <div class="cuadro_central">
+                <h1>Hello, Next.js!</h1>
+            </div>
 		</>
 	)
 }
