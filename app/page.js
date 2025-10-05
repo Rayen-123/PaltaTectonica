b2ed4d20d2dded8db249e38f7b58b5b837e01e20
Rayen-123 <rayen.aburto.caamano@gmail.com>
2025-10-05 00:13:05 -0300

    pantalla principal sin funcionalidades

--- app/page.js
+++ app/page.js
@@ -1,11 +1,69 @@
 export default function Page() {
 	return (
 		<>
-			<div class="encabezado">
-				<div class="texto_encabezado">Encabezado</div>
-			</div>
-			<div class="principal">
-				<h1>Hello, Next.js!</h1>
+			<div class="contenedor">
+				<div class="encabezado"></div>
+				<div class="main">
+					<div class="lateral">
+						<button class="calendario">Calendario</button>
+						<button class="recetas">Recetas</button>
+						<button class="lista">Lista</button>
+					</div>
+					<div class="principal">
+						<div class="semana">
+							<h2>Semana</h2>
+							<div class="dia">
+								<h5>Lunes</h5>
+								<div class="comidas">
+								<button class="comida">Almuerzo</button>
+								<button class="comida">Cena</button>
+								</div>
+							</div>
+							<div class="dia">
+								<h5>Martes</h5>
+								<div class="comidas">
+								<button class="comida">Almuerzo</button>
+								<button class="comida">Cena</button>
+								</div>
+							</div>
+							<div class="dia">
+								<h5>Miércoles</h5>
+								<div class="comidas">
+								<button class="comida">Almuerzo</button>
+								<button class="comida">Cena</button>
+								</div>
+							</div>
+							<div class="dia">
+								<h5>Jueves</h5>
+								<div class="comidas">
+								<button class="comida">Almuerzo</button>
+								<button class="comida">Cena</button>
+								</div>
+							</div>
+							<div class="dia">
+								<h5>Viernes</h5>
+								<div class="comidas">
+								<button class="comida">Almuerzo</button>
+								<button class="comida">Cena</button>
+								</div>
+							</div>
+							<div class="dia">
+								<h5>Sábado</h5>
+								<div class="comidas">
+								<button class="comida">Almuerzo</button>
+								<button class="comida">Cena</button>
+								</div>
+							</div>
+							<div class="dia">
+								<h5>Domingo</h5>
+								<div class="comidas">
+								<button class="comida">Almuerzo</button>
+								<button class="comida">Cena</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</>
 	)
