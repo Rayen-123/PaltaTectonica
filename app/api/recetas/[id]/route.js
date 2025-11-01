3ab981db1a5bc05d64207ad203605772a4987dd0
Andyyy05 <andyignaciobm@gmail.com>
2025-11-01 14:30:53 -0300

    Add files via upload

---
+++ app/api/recetas/[id]/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import { Recetas } from "../Recetario"
+
+
+// GET /api/recetas/[id]
+export function GET(req, { params }) {
+  const Id = parseInt(params.id);
+
+  const Receta = Recetas.find(r => r.Id === Id);
+
+  if (!Receta) {
+    return NextResponse.json({ error: "Receta no encontrada" }, { status: 404 });
+  }
+
+  return NextResponse.json({Receta}, {status: 200});
+}
