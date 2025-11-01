29833dccf255ed31fbda3b09c245092594567daa
Andyyy05 <andyignaciobm@gmail.com>
2025-11-01 14:26:49 -0300

    Add files via upload

---
+++ app/api/Recetario.js
@@ -0,0 +1,233 @@
+export const recetas = [
+  {
+    "Nombre" : "Pan",
+    "Id" : 0,
+    "Img" : "https://tse3.mm.bing.net/th/id/OIP.Y1O74aUF1VAUIrtGKFz0uAHaE2?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
+    "Preparacion" : "Preparacion de recetaLorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+    "Tags" : ["Vegano","Bajo Azucar", "Sin Lactosa", "Vegetariano"],
+    "Ingredientes": [
+        {
+            "Nombre" : "Harina",
+            "Cant" : 1,
+            "Unidad" : "kg",
+            "Perecible" : false
+        },
+        {
+            "Nombre" : "levadura",
+            "Cant" : 20,
+            "Unidad" : "gr",
+            "Perecible" : false
+        }
+    ]
+  },
+  {
+    "Nombre" : "Fideitos",
+    "Id" : 1,
+    "Img" : "https://th.bing.com/th/id/R.1b13707f82b6b44205c0972f531122b9?rik=PtezTq%2b2zCV8fw&pid=ImgRaw&r=0",
+    "Preparacion" : "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+     "Tags" : [],
+     "Ingredientes": [
+        {
+            "Nombre" : "Spageti",
+            "Cant" : 1,
+            "Unidad" : "pquete",
+            "Perecible" : false
+        },
+        {
+            "Nombre" : "tomate",
+            "Cant" : 3,
+            "Unidad" : "unidad",
+            "Perecible" : true
+        },
+        {
+            "Nombre" : "salchicha",
+            "Cant" : 2,
+            "Unidad" : "unidad",
+            "Perecible" : true
+        },
+        {
+            "Nombre" : "aceitunas",
+            "Cant" : 10,
+            "Unidad" : "unidad",
+            "Perecible" : true
+        },
+        {
+            "Nombre" : "queso",
+            "Cant" : 100,
+            "Unidad" : "gr",
+            "Perecible" : true
+        },
+        {
+            "Nombre" : "sal",
+            "Cant" : 2,
+            "Unidad" : "gr",
+            "Perecible" : false
+        },
+        {
+            "Nombre" : "ajo",
+            "Cant" : 2,
+            "Unidad" : "dientes",
+            "Perecible" : true
+        },
+        {
+            "Nombre" : "albaca",
+            "Cant" : 6,
+            "Unidad" : "hojas",
+            "Perecible" : true
+        }
+    ]
+  },
+  {
+    "Nombre" : "Claras a nieve ",
+    "Id" : 2,
+    "Img" : "https://tse4.mm.bing.net/th/id/OIP.yMq6pq9k75GQ_64AvfQfLwHaFj?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
+    "Preparacion" : "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+    "Tags" : ["Bajo Sodio", "Sin Gluten", "Sin Lactosa", "Vegetariano", "etc"],
+    "Ingredientes": [
+        {
+            "Nombre" : "Claras",
+            "Cant" : 200,
+            "Unidad" : "gr",
+            "Perecible" : true
+        },
+    ]
+   },
+   {
+    "Nombre" : "Empanas veganas",
+    "Id" : 3,
+    "Img" : "https://mujerypunto.com/wp-content/uploads/2017/07/empanadas-de-acelga.jpg",
+    "Preparacion" : "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+    "Tags" : ["Vegano","Bajo Azucar", "Sin Lactosa", "Vegetariano"],
+    "Ingredientes": [
+        {
+            "Nombre" : "harina",
+            "Cant" : 400,
+            "Unidad" : "gr",
+            "Perecible" : false
+        },
+        {
+            "Nombre" : "aceituna",
+            "Cant" : 6,
+            "Unidad" : "unidad",
+            "Perecible" : true
+        },
+        {
+            "Nombre" : "acelga",
+            "Cant" : 14,
+            "Unidad" : "hojas",
+            "Perecible" : true
+        },
+        {
+            "Nombre" : "ajo",
+            "Cant" : 1,
+            "Unidad" : "cabeza",
+            "Perecible" : true
+        },
+    ]
+   },
+   {
+    "Nombre" : "fideos con champiñones",
+    "Id" : 4,
+    "Img" : "https://www.recetasnestle.com.ec/sites/default/files/srh_recipes/28a5c480bf76e77846742b54fb2c1cf3.jpg",
+    "Tags" : ["Bajo Azucar", "Vegetariano"],
+    "Preparacion" : "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+    "Ingredientes": [
+        {
+            "Nombre" : "spageti",
+            "Cant" : 1,
+            "Unidad" : "paquete",
+            "Perecible" : false
+        },
+        {
+            "Nombre" : "champiñones",
+            "Cant" : 400,
+            "Unidad" : "gr",
+            "Perecible" : true
+        },
+        {
+            "Nombre" : "leche",
+            "Cant" : 1,
+            "Unidad" : "lt",
+            "Perecible" : true
+        },
+        {
+            "Nombre" : "harina",
+            "Cant" : 100,
+            "Unidad" : "gr",
+            "Perecible" : false
+        },
+        {
+            "Nombre" : "pimienta",
+            "Cant" : 5,
+            "Unidad" : "gr",
+            "Perecible" : false
+        },
+    ]
+   },
+   {
+    "Nombre" : "Crema de champiñones",
+    "Id" : 5,
+    "Img" : "https://recetasdecocina.elmundo.es/wp-content/uploads/2024/12/crema-de-champinones.jpg",
+    "Tags" : ["Bajo Azucar", "Sin Gluten", "Vegetariano"],
+    "Preparacion" : "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+    "Ingredientes": [
+        {
+            "Nombre" : "Champiñones",
+            "Cant" : 400,
+            "Unidad" : "gr",
+            "Perecible" : true
+        },
+        {
+            "Nombre" : "leche",
+            "Cant" : 1.5,
+            "Unidad" : "lt",
+            "Perecible" : true
+        },
+        {
+            "Nombre" : "ajo",
+            "Cant" : 2,
+            "Unidad" : "dientes",
+            "Perecible" : true
+        }
+    ]
+   },
+   {
+    "Nombre" : "Empanadas napolitanas",
+    "Id" : 6,
+    "Img" : "https://cdnx.jumpseller.com/lashermanaschicureo/image/56268764/resize/1200/630?1730995236",
+    "Tags" : ["Bajo Azucar"],
+    "Preparacion" : "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
+    "Ingredientes": [
+        {
+            "Nombre" : "harina",
+            "Cant" : 400,
+            "Unidad" : "gr",
+            "Perecible" : false
+        },
+        {
+            "Nombre" : "aceituna",
+            "Cant" : 6,
+            "Unidad" : "unidad",
+            "Perecible" : true
+        },
+        {
+            "Nombre" : "tomate",
+            "Cant" : 3,
+            "Unidad" : "unidad",
+            "Perecible" : true
+        },
+        {
+            "Nombre" : "salchicha",
+            "Cant" : 12,
+            "Unidad" : "unidad",
+            "Perecible" : true
+        },
+        {
+            "Nombre" : "queso",
+            "Cant" : 200,
+            "Unidad" : "gr",
+            "Perecible" : true
+        },
+    ]
+   },
+];
