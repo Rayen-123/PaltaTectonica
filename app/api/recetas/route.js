b007e32c66659c71ec4d5e843c7b763be38827e4
Andyyy05 <andyignaciobm@gmail.com>
2025-11-29 17:33:38 -0300

    Add files via upload

--- app/api/recetas/route.js
+++ app/api/recetas/route.js
@@ -1,6 +1,17 @@
-import {NextResponse} from "next/server";
-import {recetas} from "../Recetario";
+import { NextResponse } from "next/server";
+import { supabase } from "../../lib/supabase";
 
-export function GET(req, {params}){
-    return NextResponse.json(recetas, {status: 200});
+export async function GET() {
+  const { data, error } = await supabase
+    .from("recetas")
+    .select("*");
+
+  if (error) {
+    return NextResponse.json(
+      { error: "Error al obtener recetas" },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json(data, { status: 200 });
 }
